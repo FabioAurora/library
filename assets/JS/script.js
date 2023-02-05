8144c84a4f0dc101f289776e7737a622c517f446
Fabio Aurora <fabioauroradev@gmail.com>
2023-02-05 21:31:31 +0000

    Started teh JS script, with the constructor function

---
+++ assets/JS/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let myLibrary = [];
+
+// Constructor function
+function Book (title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
+
+
+function addBookToLibrary() {
+  // do stuff here
+}
