4a12f565ca7c9fa671b23b4958f41f11af6bd657
Fabio Aurora <fabioauroradev@gmail.com>
2023-02-11 07:27:55 +0000

    remodeling the existent form modal

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const openModal = document.querySelector('[data-open-modal]');
+const closeModal = document.querySelector('[data-close-modal]');
+const modal = document.querySelector('[data-modal-new]');
+
+
+function modalClose() {
+  modal.classList.add('display-none');
+}
+//listen for a click
+openModal.addEventListener('click', () => {
+  modal.classList.remove('display-none');
+})
+
+closeModal.addEventListener('click', modalClose)
+
+// listnen to outside click
+modal.addEventListener('click', clickOutside);
+
+
+//closes modal if clicked outside
+function clickOutside(e) {
+  if(e.target === modal) {
+    modalClose();
+}
+}
+
+//closes modal in ESC key
+document.addEventListener('keydown', (e) => {
+  if(e.key === 'Escape' && !modal.classList.contains('display-none')) {
+    modalClose();
+  }
+})
